8b93ea0e4b6f668c3a44e90e9a2a6f8e9a91e2e1
Bruno Siqueira <nx9010@gmail.com>
2023-10-24 16:56:31 +0100

    Improvements and corrections

--- Win32 Intune App Template Creator/Win32-Intune-App-Template-Creator.ps1
+++ Win32 Intune App Template Creator/Win32-Intune-App-Template-Creator.ps1
@@ -1,13 +1,8 @@
 ﻿<#
 TODO:
-    - Display a HELP when the script is run without the needed parameters
-    - Add the needed parameters
     - Fill the 'Deploy-Application.ps1' file with the App Name, Version, etc
-    - Try to create a PowerShell script that, given a program name installed, creates a detection script
-    - Try to download Logo files for the given application
-    - Make the script create a markdown file with some information about the application
-    - Create parameters that allow the user to choose the directory structure (One directory for each: Vendor, App and Version, or only one directory for each app. Ex.: Mozilla > Firefox > 118.0.2 OR 'Mozilla Firefox 118.0.2')
-    - Generate output information about the steps that the script is doing to give some feedback to the user
+    - Display a HELP when the script is run without the needed parameters
+    - Try to create a PowerShell script that, given a program name installed, creates a detection script for use in the Intune application
 #>
 
 [CmdletBinding()]
@@ -19,7 +14,11 @@ Param (
     [Parameter(Mandatory = $false)]
     [String]$ApplicationName = '',
     [Parameter(Mandatory = $false)]
-    [String]$ApplicationVersion = ''
+    [String]$ApplicationVersion = '',
+    [Parameter(Mandatory = $false)]
+    [switch]$JoinApplicationVendorAndApplicationName = $false,
+    [Parameter(Mandatory = $false)]
+    [switch]$JoinApplicationNameAndApplicationVersion = $false
 )
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
@@ -39,53 +38,62 @@ Try {
     [String]$psadtUrlZipFilePath = "$env:TEMP\PSAppDeployToolkit.zip"
     [String]$buildPackageFile = ""
     [String]$readmeMdFile = ""
+    [String]$mainAppPath = ""
     ##*===============================================
     ##* END VARIABLE DECLARATION
     ##*===============================================
 
-    # FOR TESTS PURPOSE ONLY
-    $DestinationPath = 'D:\Packages'
-    $ApplicationVendor = 'Mozilla'
-    $ApplicationName = 'Firefox'
-    $ApplicationVersion = '118.0.2'
-    # FOR TESTS PURPOSE ONLY
+    # If the final directory exists, stops the script
+    If (Test-Path -Path "$DestinationPath\$ApplicationVendor\$ApplicationName\$ApplicationVersion") {
+        Write-Host "There is already a directory with this version of the application in the destination path provided. The script will now terminate." -ForegroundColor Red
+        Exit 1
+    }
 
     # Creates the directory structure
+    Write-Host "Creating the directory structure..." 
     New-Item -Path "$DestinationPath" -Name "$ApplicationVendor" -ItemType Directory -Force | Out-Null
     New-Item -Path "$DestinationPath\$ApplicationVendor" -Name "$ApplicationName" -ItemType Directory -Force | Out-Null
     New-Item -Path "$DestinationPath\$ApplicationVendor\$ApplicationName" -Name "$ApplicationVersion" -ItemType Directory -Force | Out-Null
 
-    New-Item -Path "$DestinationPath\$ApplicationVendor\$ApplicationName\$ApplicationVersion" -Name "$assetsDirectoryName" -ItemType Directory -Force | Out-Null
-    New-Item -Path "$DestinationPath\$ApplicationVendor\$ApplicationName\$ApplicationVersion" -Name "$packagesDirectoryName" -ItemType Directory -Force | Out-Null
-    New-Item -Path "$DestinationPath\$ApplicationVendor\$ApplicationName\$ApplicationVersion" -Name "$sourcesDirectoryName" -ItemType Directory -Force | Out-Null
-    New-Item -Path "$DestinationPath\$ApplicationVendor\$ApplicationName\$ApplicationVersion" -Name "$ToolsDirectoryName" -ItemType Directory -Force | Out-Null
+    $mainAppPath = "$DestinationPath\$ApplicationVendor\$ApplicationName\$ApplicationVersion"
+    New-Item -Path "$mainAppPath" -Name "$assetsDirectoryName" -ItemType Directory -Force | Out-Null
+    New-Item -Path "$mainAppPath" -Name "$packagesDirectoryName" -ItemType Directory -Force | Out-Null
+    New-Item -Path "$mainAppPath" -Name "$sourcesDirectoryName" -ItemType Directory -Force | Out-Null
+    New-Item -Path "$mainAppPath" -Name "$ToolsDirectoryName" -ItemType Directory -Force | Out-Null
 
     # Define path variables
-    $buildPackageFile = "$DestinationPath\$ApplicationVendor\$ApplicationName\$ApplicationVersion\BuildPackage.cmd"
+    $buildPackageFile = "$mainAppPath\BuildPackage.cmd"
+    $readmeMdFile = "$mainAppPath\README.md"
 
     # Downloads the latest version of the Microsoft Win32 Content Prep Tool
-    $intuneWinAppUtilOutputPath = "$DestinationPath\$ApplicationVendor\$ApplicationName\$ApplicationVersion\$ToolsDirectoryName\IntuneWinAppUtil.exe"
+    Write-Host "Downloading the latest version of the Microsoft Win32 Content Prep Tool..." 
+    $intuneWinAppUtilOutputPath = "$mainAppPath\$ToolsDirectoryName\IntuneWinAppUtil.exe"
     Invoke-WebRequest -Uri $intuneWinAppUtilUrl -OutFile $intuneWinAppUtilOutputPath
 
     # Downloads the latest version of the PowerShell App Deployment Toolkit to temp folder
+    Write-Host "Downloading the latest version of the PowerShell App Deployment Toolkit..." 
     $response = Invoke-RestMethod -Uri $psadtUrl
     $psadtUrlZipFileURL = $response.assets | Where-Object{$_.name -like "*.zip"} | Select-Object -First 1 -ExpandProperty browser_download_url
     Invoke-WebRequest -Uri $psadtUrlZipFileURL -OutFile "$psadtUrlZipFilePath"
     Start-Sleep -Seconds 1
     # Extracts the content of the PSADT Zip file
+    Write-Host "Extracting the contents of the PowerShell App Deployment Toolkit..." 
     If (Test-Path -Path "$ApplicationVersion") { Remove-Item -Path "$ApplicationVersion" -Force }
     If (Test-Path -Path "$env:TEMP\PSADT") { Remove-Item -Path "$env:TEMP\PSADT" -Force -Recurse }
     [System.IO.Compression.ZipFile]::ExtractToDirectory("$psadtUrlZipFilePath", "$env:TEMP\PSADT")
     Start-Sleep -Seconds 1
     # Copy the Toolkit folder to the apropriate location
-    Copy-Item -Path "$env:TEMP\PSADT\Toolkit\*" -Destination "$DestinationPath\$ApplicationVendor\$ApplicationName\$ApplicationVersion\$sourcesDirectoryName" -Recurse -Force
+    Write-Host "Copying the contents of the PowerShell App Deployment Toolkit to the correct location..." 
+    Copy-Item -Path "$env:TEMP\PSADT\Toolkit\*" -Destination "$mainAppPath\$sourcesDirectoryName" -Recurse -Force
     Start-Sleep -Seconds 1
 
     # Rename Deploy-Application.ps1 file
+    Write-Host "Renaming the Deploy-Application.ps1 file..." 
     $AppPs1FullName = "$($ApplicationVendor)_$($ApplicationName)_$($ApplicationVersion).ps1"
-    Rename-Item -Path "$DestinationPath\$ApplicationVendor\$ApplicationName\$ApplicationVersion\$sourcesDirectoryName\Deploy-Application.ps1" -NewName "$AppPs1FullName" -Force
+    Rename-Item -Path "$mainAppPath\$sourcesDirectoryName\Deploy-Application.ps1" -NewName "$AppPs1FullName" -Force
 
     # Creates the BuildPackage.cmd file
+    Write-Host "Creating the BuildPackage.cmd file..." 
     Add-Content -Value "@ECHO OFF" -Path "$buildPackageFile" -Force
     Add-Content -Value "" -Path "$buildPackageFile" -Force
     Add-Content -Value "ECHO Removing previous package..." -Path "$buildPackageFile" -Force
@@ -98,40 +106,33 @@ Try {
     Add-Content -Value "PAUSE" -Path "$buildPackageFile" -Force
 
     # Creates the README.md file
-    $ApplicationVendor = 'Mozilla'
-    $ApplicationName = 'Firefox'
-    $ApplicationVersion = '118.0.2'
+    Write-Host "Creating the README.md file..." 
     $AppFullName = "$($ApplicationVendor)_$($ApplicationName)_$($ApplicationVersion)"
     Add-Content -Value "##$AppFullName" -Path "$readmeMdFile" -Force
     Add-Content -Value "" -Path "$readmeMdFile" -Force
     Add-Content -Value "**Description**" -Path "$readmeMdFile" -Force
-    Add-Content -Value ">PUT SOFTWARE DESCRIPTION HERE!" -Path "$readmeMdFile" -Force
+    Add-Content -Value "> <PUT SOFTWARE DESCRIPTION HERE>" -Path "$readmeMdFile" -Force
     Add-Content -Value "" -Path "$readmeMdFile" -Force
     Add-Content -Value "**Publisher:** $ApplicationVendor" -Path "$readmeMdFile" -Force
+    Add-Content -Value "**App Version:** $ApplicationVersion" -Path "$readmeMdFile" -Force
+    Add-Content -Value "**Information URL:** <INSERT THE INFORMATION URL HERE>" -Path "$readmeMdFile" -Force
+    Add-Content -Value "**Privacy URL:** <INSERT THE PRIVACY URL HERE>" -Path "$readmeMdFile" -Force
     Add-Content -Value "" -Path "$readmeMdFile" -Force
+    Add-Content -Value "**Commands:**" -Path "$readmeMdFile" -Force
+    Add-Content -Value "- **Install**" -Path "$readmeMdFile" -Force
+    Add-Content -Value "	``Deploy-Application.exe "".\$($AppFullName).ps1"" -DeploymentType 'Install' -DeployMode 'Silent'``" -Path "$readmeMdFile" -Force
+    Add-Content -Value "- **Uninstall**:" -Path "$readmeMdFile" -Force
+    Add-Content -Value "	``Deploy-Application.exe "".\$($AppFullName).ps1"" -DeploymentType 'Uninstall' -DeployMode 'Silent'``" -Path "$readmeMdFile" -Force
     Add-Content -Value "" -Path "$readmeMdFile" -Force
-    Add-Content -Value "" -Path "$readmeMdFile" -Force
-    Add-Content -Value "" -Path "$readmeMdFile" -Force
-    Add-Content -Value "" -Path "$readmeMdFile" -Force
-    Add-Content -Value "" -Path "$readmeMdFile" -Force
-    Add-Content -Value "" -Path "$readmeMdFile" -Force
-    Add-Content -Value "" -Path "$readmeMdFile" -Force
+    Add-Content -Value "**Detection Rules:**" -Path "$readmeMdFile" -Force
     Add-Content -Value "" -Path "$readmeMdFile" -Force
     Add-Content -Value "" -Path "$readmeMdFile" -Force
     Add-Content -Value "" -Path "$readmeMdFile" -Force
 
 
-    # Put some information in the Deploy-Application.ps1
-    #$installContent = Get-Content "$DestinationPath\$ApplicationVendor\$ApplicationName\$ApplicationVersion\$sourcesDirectoryName\Install.ps1"
-    #$installContent = $installContent -replace '[String]$appVendor = ''', "[String]$appVendor = '$($ApplicationVendor)'"
-    #$installContent = $installContent -replace "", ""
-    #$installContent = $installContent -replace "", ""
-    #$installContent = $installContent -replace "", ""
-    #$installContent = $installContent -replace "", ""
-    #$installContent | Set-Content "$DestinationPath\$ApplicationVendor\$ApplicationName\$ApplicationVersion\$sourcesDirectoryName\Install.ps1"
 
 
-
+    Write-Host "End of the script." 
     ##*===============================================
     ##* END SCRIPT BODY
     ##*===============================================
